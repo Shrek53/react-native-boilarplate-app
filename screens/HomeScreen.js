0d03e6055642abe6c894b98a488b4401f67bb6f9
apon <apon.ruet@gmail.com>
2018-11-24 13:15:39 +0600

    swiper implemented

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,254 +1,121 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
   ScrollView,
   StyleSheet,
   Text,
-  View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
-import CommonHeader from './partial/CommonHeader';
-
-import Swiper from 'react-native-swiper';
-class MainSwiper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-  render(){
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-           <Swiper style={styles.wrapper} showsButtons={false} showsPagination={true}>
-          {items.map(item => (
-                    <View style={styles.slide_window} key={item.name}>
-                    <Text style={styles.text}>{item.title} </Text>
-                  </View>
-                    ))}
-      </Swiper>
-      );
-    }
-  }
-}
-
-//     return (
-//       <Swiper style={styles.wrapper} showsButtons={false} showsPagination={true}>
-//         <View style={styles.slide_window}>
-//           <Text style={styles.text}>Hello Swiper</Text>
-//         </View>
-//         <View style={styles.slide_window}>
-//           <Text style={styles.text}>Beautiful</Text>
-//         </View>
-//         {items.map(item => (
-//           <View style={styles.slide_window} key={item.name}>>
-//           <Text style={styles.text}>{item.title} </Text>
-//         </View>
- 
-//           ))}
-//       </Swiper>
-//     );
-//   }
-// }
+  View
+} from "react-native";
+import { WebBrowser } from "expo";
+import CommonHeader from "./partial/CommonHeader";
+import MainSwiper from "../components/SwiperComponent";
+// import RobotInfoComponent from "../components/RobotInfoComponent";
 
 
 export default class HomeScreen extends React.Component {
-
   constructor(props) {
-      super(props);
-    }
+    super(props);
+  }
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <CommonHeader title='Home'/>
-        <MainSwiper/>
-        {/* <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-            <View style={styles.welcomeContainer}>
-              <Image
-                source={
-                  __DEV__
-                    ? require('../assets/images/robot-dev.png')
-                    : require('../assets/images/robot-prod.png')
-                }
-                style={styles.welcomeImage}
-              />
-            </View>
-
-            <View style={styles.getStartedContainer}>
-              <Text style={styles.getStartedText}>
-              This is a Boilar plate app with drawer and bottom tab navigation
-              </Text>
-            </View>
-        </ScrollView>
-       */}
+        <CommonHeader title="Home" />
+        <MainSwiper />
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   developmentModeText: {
     marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
+    color: "rgba(0,0,0,0.4)",
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: "center"
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 30
   },
   welcomeContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   },
   welcomeImage: {
     width: 100,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 3,
-    marginLeft: -10,
+    marginLeft: -10
   },
   getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+    alignItems: "center",
+    marginHorizontal: 50
   },
   homeScreenFilename: {
-    marginVertical: 7,
+    marginVertical: 7
   },
   codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+    color: "rgba(96,100,109, 0.8)"
   },
   codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 3,
-    paddingHorizontal: 4,
+    paddingHorizontal: 4
   },
   getStartedText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center"
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: { height: -3 },
         shadowOpacity: 0.1,
-        shadowRadius: 3,
+        shadowRadius: 3
       },
       android: {
-        elevation: 20,
-      },
+        elevation: 20
+      }
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20
   },
   tabBarInfoText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
   },
   navigationFilename: {
-    marginTop: 5,
+    marginTop: 5
   },
   helpContainer: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center"
   },
   helpLink: {
-    paddingVertical: 15,
+    paddingVertical: 15
   },
   helpLinkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: "#2e78b7"
   },
-   slide_window: {
-       flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center',
-       backgroundColor: '#fff',
-     },
-     text: {
-       color: '#000',
-       fontSize: 30,
-       fontWeight: 'bold',
-     }
 });
